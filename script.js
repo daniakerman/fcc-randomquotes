af5c085c1fb69589658630c9e0b1db984e1a1369
Daniella Åkerman <daniella.aum@gmail.com>
2017-05-17 16:20:41 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function getQuote() {
+
+    var quotes = ["Stay on target. – Gold Five", "Your eyes can deceive you. Don’t trust them. – Obi-Wan Kenobi", "I find your lack of faith disturbing. – Darth Vader", "Do. Or do not. There is no try. – Yoda", "Your focus determines your reality. – Qui-Gon Jinn", "It’s not my fault. – Han Solo"]
+
+  
+} 
