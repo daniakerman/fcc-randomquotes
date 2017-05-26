ceee4207264087c155ece1d3989ae77cab4f6d60
Daniella <daniella.aum@gmail.com>
2017-05-26 22:50:27 +0300

    added hover for twitter button

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
-$( document ).ready(function() {
-  // Handler for .ready() called.
+$(document).ready(function () {
+    // Handler for .ready() called.
 
-var quotes = ["Stay on target. &mdash; Gold Five",
-    "Your eyes can deceive you. Don&#39;t trust them. &mdash; Obi-Wan Kenobi",
-    "I find your lack of faith disturbing. &mdash; Darth Vader",
-    "Do. Or do not. There is no try. &mdash; Yoda",
-    "Your focus determines your reality. &mdash; Qui-Gon Jinn",
-    "It&#39;s not my fault. &mdash; Han Solo",
-    "It&#39;s not my fault. &mdash; Han Solo",
-    "It&#39;s not my fault. &mdash; Han Solo",
-    "It&#39;s not my fault. &mdash; Han Solo",
-    "It&#39;s not my fault. &mdash; Han Solo"]
+    var quotes = ["Stay on target. &mdash; Gold Five",
+        "Your eyes can deceive you. Don&#39;t trust them. &mdash; Obi-Wan Kenobi",
+        "I find your lack of faith disturbing. &mdash; Darth Vader",
+        "Do. Or do not. There is no try. &mdash; Yoda",
+        "Your focus determines your reality. &mdash; Qui-Gon Jinn",
+        "It&#39;s not my fault. &mdash; Han Solo",
+        "It&#39;s not my fault. &mdash; Han Solo",
+        "It&#39;s not my fault. &mdash; Han Solo",
+        "It&#39;s not my fault. &mdash; Han Solo",
+        "It&#39;s not my fault. &mdash; Han Solo"]
 
 
-// Assigned/bind click event to the handler/callback
-$("#quoteButton").click(function () {
+    // Assigned/bind click event to the handler/callback
+    $("#quoteButton").click(function () {
 
-    // Generated random number from 0 - 10
-    var randomKey = Math.floor(Math.random() * 10);
+        // Generated random number from 0 - 10
+        var randomKey = Math.floor(Math.random() * 10);
 
-    // getting single quote by generated key
-    var singleQuote = quotes[randomKey];
+        // getting single quote by generated key
+        var singleQuote = quotes[randomKey];
 
-    // put single quote in placeholder
-    $('#randomQuote').html(singleQuote);
-    
-    // Tweet out a quote
-    $('.twitter-share-button').attr("href", 'https://twitter.com/intent/tweet?text=' + singleQuote);
+        // put single quote in placeholder
+        $('#randomQuote').html(singleQuote);
 
-    // Show tweet
-    $('.twitter-share-button').show();
-});
-    
+        // Tweet out a quote
+        $('.twitter-share-button').attr("href", 'https://twitter.com/intent/tweet?text=' + singleQuote);
+
+        // Show tweet
+        $('.twitter-share-button').show();
+    });
+
+    // Animation Effect on hover
+    $(".twitter-share-button").hover(
+        function () {
+            $(this).animate({top: "-15"});
+        },
+        function () {
+            $(this).animate({top: "0"});
+        }
+    );
+
+// ready
 });
